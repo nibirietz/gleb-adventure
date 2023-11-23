4d6bbdf21d3267a34a367f0679861f7fc9e076d8
nibirietz <nibirietz@gmail.com>
2023-11-23 14:02:57 +0300

    IMAGES!!

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import flet
 import csv
 from urllib.request import urlretrieve
-import sys
 
 
 class MainWindow:
     def __init__(self):
+        self.picture = flet.Container(
+            image_src="data/images/yurta.png",
+            image_fit=flet.ImageFit.COVER,
+            expand=True,
+        )
         flet.app(target=self.main_loop)
 
     def main_loop(self, page: flet.Page):
@@ -19,10 +23,15 @@ class MainWindow:
                 for answer in self.dialogue.answers
             ]
         )
-        self.page.add(self.question_text, self.answers_button)
 
-    def update_view(self, question: str, answers: list[str]):
-        self.page.remove(self.question_text, self.answers_button)
+        self.page.add(self.question_text, self.answers_button, self.picture)
+
+    def update_view(self, question: str, answers: list[str], picture=None):
+        self.page.remove(self.question_text, self.answers_button, self.picture)
+        if picture != None:
+            self.picture = flet.Container(
+                image_src=picture, image_fit=flet.ImageFit.COVER, expand=True
+            )
         self.question_text = flet.Text(question)
         self.answers_button = flet.Row(
             [
@@ -30,7 +39,7 @@ class MainWindow:
                 for answer in answers
             ]
         )
-        self.page.add(self.question_text, self.answers_button)
+        self.page.add(self.question_text, self.answers_button, self.picture)
 
 
 class Script:
@@ -38,19 +47,10 @@ class Script:
         self.window = window
         with open("data/script.csv", encoding="utf-8") as csvfile:
             self.data = list(csv.DictReader(csvfile))
-            print(self.data)
 
         self.first_question = self.data[0]["Вопрос"]
         self.desciption = self.data[0]["Описание"]
         self.answers = self.parse_question(self.data[0]["Ответы"])
-
-    def download_files(self):
-        if "test" not in sys.argv:
-            with open("data/urls.txt", "r") as file:
-                try:
-                    urlretrieve(file.readline())
-                except FileExistsError as e:
-                    print(e)
 
     def parse_question(self, line: str) -> list[str]:
         if line == "@":
@@ -74,10 +74,17 @@ class Script:
             if row["Вопрос"] == previous_answer:
                 self.desciption = row["Описание"]
                 self.answers = self.parse_question(row["Ответы"])
-                self.window.update_view(self.desciption, self.answers)
+                self.window.update_view(
+                    self.desciption, self.answers, self.get_image(row["Картинка"])
+                )
                 return
         else:
             self.window.update_view("Дальше бога нет.", ["Выйти", "Начать заново"])
+
+    def get_image(self, image: str) -> str | None:
+        if image == "@":
+            return None
+        return "data/images/" + image
 
 
 def main():
